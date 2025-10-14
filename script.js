8cd35fdc4bd33a29b598f2aaeb9205181ac0be2e
2xbarrel <barrelthegreat@gmail.com>
2025-10-14 18:43:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let card = document.getElementById("card");
+let cardname = document.getElementById("cardname");
+let telling = document.getElementById("telling");
+let button = document.getElementById("button");
+
+button.addEventListener("click", e => {
+    let arcana = Math.floor(Math.random() * 22);
+    let readingsNum = Math.floor(Math.random() * 10);
+    card.src = `cards/${reading[arcana].name}.png`;
+    cardname.innerHTML = reading[arcana].name;
+    telling.innerHTML = reading[arcana].readings[readingsNum];
+});
